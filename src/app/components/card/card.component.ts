07cea28c6e1c424e08dd5a6b6e1253980992e0ec
Jose-Ilafaya <ilafayajose@gmail.com>
2022-05-27 11:33:53 -0300

    diseño de cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
 
