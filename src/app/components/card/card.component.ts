6dc7698a48a6582ecc97c81c480f672a99b37717
Jose-Ilafaya <ilafayajose@gmail.com>
2022-06-09 11:07:15 -0300

    jueves

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-// public infoRelojes:                             
+// public infoRelojes: Relojes[]=[];
+// reloj!:Relojes                             
 //para agregar al constructor (private relojService:RelojService)
-//ngOnInit{this.relojservice.getDataReloj().subscribe((resp:RelojDescripcion))}
+//ngOnInit{this.relojservice.getDataReloj().subscribe((resp:RelojDescripcion[]=>{this.infoRelojes=resp;}))}
   constructor() { }
 
   ngOnInit(): void {
--- src/app/services/reloj-service.service.ts
+++ src/app/services/reloj-service.service.ts
@@ -3,15 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Reloj } from '../model/reloj';
 import { Observable } from 'rxjs';
 
-@Injectable({
+@Injectable({ //usa decorador @ 
   providedIn: 'root'
 })
 export class RelojServiceService {
 
-  private relojUrl= "assets/data/reloj-data.json"
+  private relojUrl= "assets/data/reloj-data.json" 
 
   constructor(private http:HttpClient) { }
-  getDataReloj():Observable<any>{
+  
+  getDataReloj():Observable<any>{ //
     return this.http.get<Reloj[]>(this.relojUrl)
   }
 }
