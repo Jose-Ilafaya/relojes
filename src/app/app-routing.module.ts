c53673bb7aab7d21d206ef54b35d690121f9e197
Jose-Ilafaya <ilafayajose@gmail.com>
2022-06-30 07:37:28 -0300

    finde

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ColeccionesComponent } from './components/colecciones/colecciones.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'', component:HomeComponent},
+  {path:'collections',component:ColeccionesComponent}
 ];
 
 @NgModule({
