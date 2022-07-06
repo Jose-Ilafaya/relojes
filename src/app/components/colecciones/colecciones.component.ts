b9faf6efeab2405ea78910b929f46030dc0c6b17
Jose-Ilafaya <ilafayajose@gmail.com>
2022-07-06 00:53:17 -0300

    inclusion del video y modificaciones necesarias

--- src/app/components/colecciones/colecciones.component.ts
+++ src/app/components/colecciones/colecciones.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./colecciones.component.css']
 })
 export class ColeccionesComponent implements OnInit {
-
+  private colecciones:[]=[];
   constructor() { }
 
   ngOnInit(): void {
